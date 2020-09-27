dbc432ab061ab49f905968ccc44370f2021715ad
vagrant <vagrant@localhost.localdomain>
2020-09-27 06:48:24 +0000

    インデント修正など

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -2,6 +2,6 @@ class Gym < ApplicationRecord
 	has_many :machines, dependent: :destroy
 	accepts_nested_attributes_for :machines
 	has_many :gym_comments,dependent: :destroy
-	geocoded_by :address
-   	after_validation :geocode, if: :address_changed?
+	# geocoded_by :address
+ #   	after_validation :geocode, if: :address_changed?
 end
